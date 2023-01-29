bd9e5ce8b6f3b56167ba83248cc4a8a6875f5a99
shamilhuseinov <shamilrh@code.edu.az>
2023-01-29 20:11:50 +0400

    homework is added

---
+++ Encapsulation/Invoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Encapsulation
+{
+	public class Invoice
+	{
+		int account;
+		string customer;
+		string provider;
+
+		public Invoice(int account, string customer, string provider)
+		{
+			this.account = account;
+			this.customer = customer;
+			this.provider = provider;
+		}
+
+		public string Article { get; set; }
+
+		public int Quantity { get; set; }
+
+		public decimal Price { get; set; }
+
+		public void CostCalculation(bool needEdv)
+        {
+            if (needEdv==true)
+            {
+                Console.WriteLine($"Məhsul: {Article}\nMəhsulun dəyəri: {Price*Quantity*(decimal)1.18}");
+            }
+            else
+            {
+                Console.WriteLine($"Məhsul: {Article}\nMəhsulun dəyəri: {Price * Quantity}");
+            }
+        }
+    }
+}
+
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Encapsulation;
+
+namespace test
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Invoice inv = new Invoice(678904, "Alex", "Foxtrot");
+            inv.Article = "USB-hab";
+            inv.Quantity = 6;
+            inv.Price = 30;
+            inv.CostCalculation(true);
+        }
+    }
+}
